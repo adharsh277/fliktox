f3a6280499f9f6b95464f9185fdabd496849042f
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-04-05 14:06:28 +0000

    added  backend/src/server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,17 @@ import { createServer } from "http";
 import { app } from "./app.js";
 import { env } from "./config/env.js";
 import { registerChatSocket } from "./socket/chatSocket.js";
+import { startTrendingRefreshLoop } from "./services/trendingCache.js";
 
 const server = createServer(app);
 registerChatSocket(server);
+startTrendingRefreshLoop({
+  refreshMinutes: env.trendingRefreshMinutes,
+  limit: env.trendingLimit,
+  windowDays: env.trendingWindowDays
+}).catch((error) => {
+  console.error("Initial trending refresh failed", error);
+});
 
 server.listen(env.port, () => {
   console.log(`Fliktox backend running on http://localhost:${env.port}`);
